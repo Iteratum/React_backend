2c284df925b6b6e43acb04776d5d13866bb8043e
Iteratum <103502686+Iteratum@users.noreply.github.com>
2023-07-05 22:02:11 +0100

    setting up allowed host for 'Render'

--- DBlog/settings.py
+++ DBlog/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME: ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
