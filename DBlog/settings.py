8bbb47c3ed75b8e4e1da14d896baa717c59c6888
Iteratum <103502686+Iteratum@users.noreply.github.com>
2023-07-05 16:51:22 +0100

    changes to settings.py

--- DBlog/settings.py
+++ DBlog/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    'whitenoise,middleware.WhitenoiseMiddleware',
+    'whitenoise.middleware.WhitenoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
